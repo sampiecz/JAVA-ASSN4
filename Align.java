b90f15242842def0ba562a39507ee6fa3d257c70
Samuel Piecz <samuel.piecz@gmail.com>
2019-03-06 14:28:43 -0600

    Initial commit.

---
+++ Align.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Align 
+{
+    // can do with three jpanels
+    public static void main(String[] args)
+    {
+        AlignFrame alignframe
+    }
+
+}
---
+++ AlignFrame.java
@@ -0,0 +1,6 @@
+import
+
+public AlignFrame()
+{
+
+}
